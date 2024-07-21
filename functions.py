de51e543b44d295662637e666d840e2b3f5da824
pra-navv <pranavwagholikar09@gmail.com>
2024-07-21 20:52:13 +0530

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,24 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return
+     the list of to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
+"""we are printing out get_todos just to check if it is 
+giving correct desired output.
+we dont want to print out this statement when we run
+th main file."""
